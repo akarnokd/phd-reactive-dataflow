01a8946e81c7883ef135282d5a1f17fe22d279c1
akarnokd <akarnokd@gmail.com>
2015-02-18 13:21:57 +0100

    Initial project commit

---
+++ src/test/java/com/github/akarnokd/msmr/TestDependencies.java
@@ -0,0 +1,20 @@
+package com.github.akarnokd.msmr;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import rx.Observable;
+
+public class TestDependencies {
+
+    @Test
+    public void test() {
+        assertEquals((Integer)1, Observable.just(1).toBlocking().first());
+    }
+
+    public void testLambda() {
+        Runnable r = System.out::println;
+        r.run();
+    }
+}
